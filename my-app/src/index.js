8bb24c6bc8cdbe45efacc588c4d7a8caff0b3975
Jose Laurito <jigolaurito93@yahoo.com>
2023-09-19 16:24:53 -0500

    trying inline styling

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
+
 const BookList = () => (
-  <section>
+  <section className="booklist">
     <Book />
     <Book />
     <Book />
@@ -10,7 +12,7 @@ const BookList = () => (
 );
 
 const Book = () => (
-  <article>
+  <article className="book">
     <Img />
     <Title />
     <Author />
@@ -19,12 +21,16 @@ const Book = () => (
 
 const Img = () => (
   <img
-    src="https://m.media-amazon.com/images/I/71HbYElfY0L._SL1500_.jpg"
+    src="https://m.media-amazon.com/images/I/51HSkTKlauL._SY445_SX342_.jpg"
     alt="Harry Potter 1 Book"
   />
 );
 const Title = () => <h2>Harry Potter and the Sorcerer's Stone</h2>;
-const Author = () => <h4>J.K. Rowling</h4>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75", marginTop: "0.5rem" }}>
+    J.K. Rowling
+  </h4>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
