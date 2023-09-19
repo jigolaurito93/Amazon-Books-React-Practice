3619e7cdfe7888a75047a25371384f4a4abe6f0f
Jose Laurito <jigolaurito93@yahoo.com>
2023-09-19 16:08:56 -0500

    created a section for bookList with subcomponents

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-  return (
-    <>
-      <h1 className="someValue">hello</h1>
-      <Person />
-    </>
-  );
-}
-
-const Person = () => (
-  <div>
-    <h2>Im Jigoo</h2>
-  </div>
+const BookList = () => (
+  <section>
+    <Book />
+    <Book />
+    <Book />
+  </section>
 );
+
+const Book = () => (
+  <article>
+    <Img />
+    <Title />
+    <Author />
+  </article>
+);
+
+const Img = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/71HbYElfY0L._SL1500_.jpg"
+    alt="Harry Potter 1 Book"
+  />
+);
+const Title = () => <h2>Harry Potter and the Sorcerer's Stone</h2>;
+const Author = () => <h4>J.K. Rowling</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
